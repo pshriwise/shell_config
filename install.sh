6cc18a8811a2e4502364393d9707efbd5adc5f53
Baptiste MOUGINOT <mouginot@d128104184078.engr.wisc.edu>
2016-03-25 08:41:04 -0500

    include gitconfig and update instal.sh accordingly

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ git clone git@github.com:Baaaaam/zsh_config.git .zsh_config
 git clone git@github.com:Baaaaam/oh-my-zsh.git oh-my-zsh
 mv .zshrc .zshrc_bak
 ln -s .zsh_config/zshrc .zshrc
+ln -s .zsh_config/git_config .gitconfig
