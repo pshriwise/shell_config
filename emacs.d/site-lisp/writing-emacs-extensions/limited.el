2fd4e7e89eb09d84fd9b2fd6319a33115dceae82
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-09-12 14:26:13 -0500

    removing emacs

--- emacs.d/site-lisp/writing-emacs-extensions/limited.el
+++
@@ -1,9 +0,0 @@
-(defmacro limited-save-excursion (&rest subexprs)
-  "Like save-excursion, but only restores point."
-  (let ((orig-point-symbol (make-symbol "orig-point")))
-    `(let ((,orig-point-symbol (point-marker)))
-       (unwind-protect
-           (progn ,@subexprs)
-         (goto-char ,orig-point-symbol)))))
-
-(provide 'limited)
