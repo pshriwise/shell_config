43c6e0900ff8cc0227f58964f280c6d63dabc4c1
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-12-05 11:11:28 -0600

    add clangformat into install

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ git clone https://github.com/Baaaaam/oh-my-zsh.git oh-my-zsh
 git clone https://github.com/powerline/fonts.git
 
 git clone http://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone http//github.com/rhysd/vim-clang-format.git vim/vim-clang-format
+
 
 # Backing-up the existing file
 mv ../.zshrc ../.zshrc_bak
