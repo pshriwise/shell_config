d1c8246beaca4055ec42d6e31207901f091ac5be
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-12-24 23:40:11 +0000

    fix install

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@ git clone https://github.com/Baaaaam/oh-my-zsh.git oh-my-zsh
 git clone https://github.com/powerline/fonts.git
 
 git clone http://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone http//github.com/rhysd/vim-clang-format.git vim/vim-clang-format
+git clone http://github.com/rhysd/vim-clang-format.git vim/vim-clang-format
 
 
 # Backing-up the existing file
-mv ../.zshrc ../.zshrc_bak
-mv ../.gitconfig ../.gitconfig_bak
-mv ../.vimrc .vimrc_bak
-mv ../.vim .../vim_bak
+mv ~/.zshrc ~/.zshrc_bak
+mv ~/.gitconfig ~/.gitconfig_bak
+mv ~/.vimrc ~/.vimrc_bak
+mv ~/.vim ~./vim_bak
 
 # Installing the new config
-ln -s ../.zsh_config/zshrc ../.zshrc
-ln -s ../.zsh_config/gitconfig ../.gitconfig
-ln -s ../.zsh_config/vim ../.vim
-ln -s ../.zsh_config/vimrc ../.vimrc
-ln -s ../.zsh_config/git-change-url ../.local/bin/
+ln -s ~/.zsh_config/zshrc ~/.zshrc
+ln -s ~/.zsh_config/gitconfig ~/.gitconfig
+ln -s ~/.zsh_config/vim ~/.vim
+ln -s ~/.zsh_config/vimrc ~/.vimrc
+ln -s ~/.zsh_config/git-change-url ~/.local/bin/
