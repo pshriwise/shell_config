bcb20b6cd1d4cfaef49d85fa9d7997d8a9347c22
MOUGINOT B <mouginot.baptiste@gmail.com>
2017-01-22 16:43:26 -0600

    add change url for git when creating git repo from inside the folder

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Getting Omzsh & fonts for iterm
-git clone https://github.com/Baaaaam/oh-my-zsh.git oh-my-zsh
+git clone https://github.com/bam241/oh-my-zsh.git oh-my-zsh
 git clone https://github.com/powerline/fonts.git
 
 git clone http://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
