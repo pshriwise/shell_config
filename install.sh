50bb5999f0ec8e17e0b6050400e8e7f1e31a3e09
pshriwise <pshriwise@gmail.com>
2017-02-14 15:57:32 -0600

    Removing stuff I don't use from the install script.

--- install.sh
+++ install.sh
@@ -10,13 +10,7 @@ git clone http://github.com/rhysd/vim-clang-format.git vim/vim-clang-format
 
 # Backing-up the existing file
 mv ~/.zshrc ~/.zshrc_bak
-mv ~/.gitconfig ~/.gitconfig_bak
-mv ~/.vimrc ~/.vimrc_bak
-mv ~/.vim ~./vim_bak
 
 # Installing the new config
 ln -s ~/.zsh_config/zshrc ~/.zshrc
-ln -s ~/.zsh_config/gitconfig ~/.gitconfig
-ln -s ~/.zsh_config/vim ~/.vim
-ln -s ~/.zsh_config/vimrc ~/.vimrc
 ln -s ~/.zsh_config/git-change-url ~/.local/bin/
