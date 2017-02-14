cb1e24c930b6d83179b5b0f35ef65efc4fe4c631
shriwise <pshriwise@gmail.com>
2017-02-14 16:33:58 -0600

    Moving to submodules.

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Getting Omzsh & fonts for iterm
-git clone https://github.com/bam241/oh-my-zsh.git oh-my-zsh
-git clone https://github.com/powerline/fonts.git
-
-git clone http://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone http://github.com/rhysd/vim-clang-format.git vim/vim-clang-format
+# Grab all submodules
+git submodule update --init --recursive
 
 
 # Backing-up the existing file
